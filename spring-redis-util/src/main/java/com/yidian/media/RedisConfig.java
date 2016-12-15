8f7d9c6c0e69a38ecbfb3570ea38617958f881bb
zhangbing <zhangbing@yidian-inc.com>
2016-12-15 10:47:20 +0800

    spring-redis-util

---
+++ spring-redis-util/src/main/java/com/yidian/media/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.yidian.media;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+/**
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @date 16/4/15 下午3:19.
+ * @blog http://blog.didispace.com
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+//    @Bean
+//    public RedisTemplate<String, User> redisTemplate(RedisConnectionFactory factory) {
+//        RedisTemplate<String, User> template = new RedisTemplate<String, User>();
+//        template.setConnectionFactory(jedisConnectionFactory());
+//        template.setKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new RedisObjectSerializer());
+//        return template;
+//    }
+}
---
+++ spring-redis-util/src/test/java/com/yidian/media/ApplicationTests.java
@@ -0,0 +1,33 @@
+package com.yidian.media;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+public class ApplicationTests {
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
+	@Autowired
+	private RedisTemplate redisTemplate;
+
+	@Test
+	public void test() throws Exception {
+
+		// 保存字符串
+		stringRedisTemplate.opsForValue().set("aaa", "111");
+		System.out.println(stringRedisTemplate.opsForValue().get("aaa"));
+
+		redisTemplate.opsForValue().set("bbb", new User("zhangbing", 18));
+		User user = (User) redisTemplate.opsForValue().get("bbb");
+//		Assert.assertEquals("111", );
+	}
+}
